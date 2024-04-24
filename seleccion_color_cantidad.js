2493383222e2fd683c1534192e8a4d22779dddc1
Miguel Martinez <147992805+miguel2007890@users.noreply.github.com>
2024-04-24 22:43:44 +0200

    En la seccion dos he hecho que cuando clcikes en el producto te lleve a elegir

---
+++ seleccion_color_cantidad.js
@@ -0,0 +1,13 @@
+// Función para volver a la sección de productos desde la página de selección de producto
+function volverAProductos() {
+    // Redirigir a la sección de productos
+    window.location.href = 'index.html';
+}
+
+// Función para agregar el producto al carrito
+function agregarAlCarrito() {
+    // Código para agregar el producto al carrito
+    alert("¡Producte agregat al carret!");
+    // Redirigir a la página de productos
+    window.location.href = 'index.html';
+}
