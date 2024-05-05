b23add8a7f16955162bcaf94d3ec325f16cccc25
Miguel Martinez <147992805+miguel2007890@users.noreply.github.com>
2024-05-06 00:06:12 +0200

    Update seleccion_color_cantidad.js

--- seleccion_color_cantidad.js
+++ seleccion_color_cantidad.js
@@ -1,13 +1,13 @@
 // Función para volver a la sección de productos desde la página de selección de producto
 function volverAProductos() {
     // Redirigir a la sección de productos
-    window.location.href = 'index.html';
+    window.location.href = 'prueba.html';
 }
 
 // Función para agregar el producto al carrito
 function agregarAlCarrito() {
     // Código para agregar el producto al carrito
-    alert("¡Producte agregat al carret!");
+    alert("Producte agregat al carret!");
     // Redirigir a la página de productos
-    window.location.href = 'index.html';
+    window.location.href = 'prueba.html'; 
 }
