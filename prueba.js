9389c5adeeeafa004f3833552dfbc8eaa0c9463a
Miguel Martinez <147992805+miguel2007890@users.noreply.github.com>
2024-05-06 00:03:20 +0200

    Update and rename index.js to prueba.js

--- prueba.js
+++ prueba.js
@@ -3,6 +3,7 @@ function mostrarPaginaPrincipal() {
     document.getElementById("seccio_0").style.display = "none"; // Ocultar la sección de inicio de sesión
     document.getElementById("menu").style.display = "flex"; // Mostrar el menú
     document.getElementById("seccio_1").style.display = "flex"; // Mostrar la página principal
+    document.getElementById("superior").style.display = "none";
 }
 
 // Función para mostrar la sección de inicio de sesión al principio
@@ -11,11 +12,10 @@ function mostrarInicioDeSesion() {
     document.getElementById("seccio_1").style.display = "none"; // Ocultar la página principal
     document.getElementById("seccio_0").style.display = "flex"; // Mostrar la sección de inicio de sesión
 }
-
-// Manejador de clics para el botón de iniciar sesión
-document.getElementById("inici_sessio").addEventListener("click", function() {
+function Enviar(){
     mostrarPaginaPrincipal();
-});
+}
+
 // Manejadores de clics para los botones del menú
 document.getElementById("boto_1").addEventListener("click", function() {
     mostrarPaginaPrincipal();
@@ -25,7 +25,23 @@ document.getElementById("boto_1").addEventListener("click", function() {
     document.getElementById("seccio_4").style.display = "none";
     document.getElementById("seccio_0").style.display = "none"; // Ocultar la sección de inicio de sesión si es necesario
     document.getElementById("seccio_5").style.display = "none";
+    document.getElementById("boto_1").classList.add("seccion-activa");
+    document.getElementById("boto_2").classList.remove("seccion-activa");
+    document.getElementById("boto_3").classList.remove("seccion-activa");
+    document.getElementById("boto_4").classList.remove("seccion-activa");
+    document.getElementById("boto_5").classList.remove("seccion-activa");
 });
+
+document.getElementById("botellaButton").addEventListener("click", function() {
+    // Ocultar otras secciones y mostrar la sección correspondiente
+    document.getElementById("seccio_1").style.display = "none";
+    document.getElementById("seccio_3").style.display = "none";
+    document.getElementById("seccio_4").style.display = "none";
+    document.getElementById("seccio_5").style.display = "none";
+    document.getElementById("seccio_2").style.display = "none"; // Mostrar la sección de productos
+    document.getElementById("seccio_6").style.display = "flex";
+});
+
 
 document.getElementById("boto_2").addEventListener("click", function() {
     document.getElementById("seccio_2").style.display = "flex"; // Mostrar la sección de productos
@@ -34,6 +50,12 @@ document.getElementById("boto_2").addEventListener("click", function() {
     document.getElementById("seccio_4").style.display = "none";
     document.getElementById("seccio_0").style.display = "none"; // Ocultar la sección de inicio de sesión si es necesario
     document.getElementById("seccio_5").style.display = "none";
+    document.getElementById("boto_2").classList.add("seccion-activa");
+    document.getElementById("boto_1").classList.remove("seccion-activa");
+    document.getElementById("boto_3").classList.remove("seccion-activa");
+    document.getElementById("boto_4").classList.remove("seccion-activa");
+    document.getElementById("boto_5").classList.remove("seccion-activa");
+    
 });
 
 document.getElementById("boto_3").addEventListener("click", function() {
@@ -43,6 +65,11 @@ document.getElementById("boto_3").addEventListener("click", function() {
     document.getElementById("seccio_4").style.display = "none";
     document.getElementById("seccio_0").style.display = "none"; // Ocultar la sección de inicio de sesión si es necesario
     document.getElementById("seccio_5").style.display = "none";
+    document.getElementById("boto_3").classList.add("seccion-activa");
+    document.getElementById("boto_1").classList.remove("seccion-activa");
+    document.getElementById("boto_2").classList.remove("seccion-activa");
+    document.getElementById("boto_4").classList.remove("seccion-activa");
+    document.getElementById("boto_5").classList.remove("seccion-activa");
 });
 
 document.getElementById("boto_4").addEventListener("click", function() {
@@ -52,6 +79,11 @@ document.getElementById("boto_4").addEventListener("click", function() {
     document.getElementById("seccio_3").style.display = "none";
     document.getElementById("seccio_0").style.display = "none"; // Ocultar la sección de inicio de sesión si es necesario
     document.getElementById("seccio_5").style.display = "none";
+    document.getElementById("boto_4").classList.add("seccion-activa");
+    document.getElementById("boto_1").classList.remove("seccion-activa");
+    document.getElementById("boto_2").classList.remove("seccion-activa");
+    document.getElementById("boto_3").classList.remove("seccion-activa");
+    document.getElementById("boto_5").classList.remove("seccion-activa");
 });
 document.getElementById("boto_5").addEventListener("click", function() {
     document.getElementById("seccio_4").style.display = "none"; // Mostrar la sección de miembros del grupo
@@ -60,10 +92,18 @@ document.getElementById("boto_5").addEventListener("click", function() {
     document.getElementById("seccio_3").style.display = "none";
     document.getElementById("seccio_0").style.display = "none"; // Ocultar la sección de inicio de sesión si es necesario
     document.getElementById("seccio_5").style.display = "flex";
+    document.getElementById("boto_5").classList.add("seccion-activa");
+    document.getElementById("boto_1").classList.remove("seccion-activa");
+    document.getElementById("boto_2").classList.remove("seccion-activa");
+    document.getElementById("boto_3").classList.remove("seccion-activa");
+    document.getElementById("boto_4").classList.remove("seccion-activa");
+
 });
 // Manejador de clics para el botón "tornar" en cada página
-document.getElementById("tornar").addEventListener("click", function() {
-    mostrarPaginaPrincipal(); // Volver a la página principal
+document.querySelectorAll("#tornar").forEach(function(button) {
+    button.addEventListener("click", function() {
+        mostrarPaginaPrincipal(); // Volver a la página principal
+    });
 });
 
 // Al cargar la página, mostrar la página de inicio de sesión
@@ -73,6 +113,11 @@ document.querySelectorAll("#tornar").forEach(function(button) {
     button.addEventListener("click", function() {
         document.querySelectorAll(".seccio").forEach(function(section) {
             section.style.display = "none"; // Ocultar todas las secciones
+            document.getElementById("boto_5").classList.remove("seccion-activa");
+            document.getElementById("boto_1").classList.remove("seccion-activa");
+            document.getElementById("boto_2").classList.remove("seccion-activa");
+            document.getElementById("boto_3").classList.remove("seccion-activa");
+            document.getElementById("boto_4").classList.remove("seccion-activa");
         });
         document.getElementById("seccio_1").style.display = "flex"; // Mostrar solo la página principal
     });
@@ -95,3 +140,7 @@ function seleccionarProducto(imagen, nombre) {
     localStorage.setItem('productoSeleccionado', JSON.stringify({ imagen: imagen, nombre: nombre }));
     window.location.href = 'seleccion_color_cantidad.html';
 }
+
+document.getElementById("botellaButton").addEventListener("click", function() {
+    window.location.href = 'seleccion_color_cantidad.html';
+});
