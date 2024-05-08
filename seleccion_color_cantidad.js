53d0b1ddd1150df20abd63322a05faa912efe365
Miguel Martinez <147992805+miguel2007890@users.noreply.github.com>
2024-05-08 16:42:57 +0200

    Update seleccion_color_cantidad.js

--- seleccion_color_cantidad.js
+++ seleccion_color_cantidad.js
@@ -1,13 +1,42 @@
 // Función para volver a la sección de productos desde la página de selección de producto
 function volverAProductos() {
     // Redirigir a la sección de productos
-    window.location.href = 'prueba.html';
+    window.location.href = 'prueba.html#productos';
 }
 
 // Función para agregar el producto al carrito
 function agregarAlCarrito() {
     // Código para agregar el producto al carrito
-    alert("Producte agregat al carret!");
+    alert("¡Producte agregat al carret!");
     // Redirigir a la página de productos
-    window.location.href = 'prueba.html'; 
+    window.location.href = 'prueba.html';
+}
+
+procuctoSeleccionado = JSON.parse(localStorage.getItem("productoSeleccionado")).nombre;
+if (procuctoSeleccionado == "botella") {
+    document.getElementById("producto-botella").style.display = "flex";
+    document.getElementById("producto-cartro").style.display = "none";
+    document.getElementById("producto-bosses").style.display = "none";
+    document.getElementById("producto-gots").style.display = "none";
+}
+
+if (procuctoSeleccionado == "cartro.reutilitzable") {
+    document.getElementById("producto-botella").style.display = "none";
+    document.getElementById("producto-cartro").style.display = "flex";
+    document.getElementById("producto-bosses").style.display = "none";
+    document.getElementById("producto-gots").style.display = "none";
+}
+
+if (procuctoSeleccionado == "bosses.entrepa") {
+    document.getElementById("producto-botella").style.display = "none";
+    document.getElementById("producto-cartro").style.display = "none";
+    document.getElementById("producto-bosses").style.display = "flex";
+    document.getElementById("producto-gots").style.display = "none";
+}
+
+if (procuctoSeleccionado == "gots.cafe") {
+    document.getElementById("producto-botella").style.display = "none";
+    document.getElementById("producto-cartro").style.display = "none";
+    document.getElementById("producto-bosses").style.display = "none";
+    document.getElementById("producto-gots").style.display = "flex";
 }
